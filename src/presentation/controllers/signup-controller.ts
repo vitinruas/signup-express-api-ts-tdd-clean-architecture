07acc10ee970bee5e561c1c1219ec25a55d26762
João Vitor Santos <joao_vitor.personal@outlook.com>
2022-06-14 20:25:08 -0300

    feat: ensure SignUpController returns a 422 error code if invalid email is provided

--- src/presentation/controllers/signup-controller.ts
+++ src/presentation/controllers/signup-controller.ts
@@ -2,9 +2,15 @@ import { InvalidParamError } from '../errors/invalid-param-error'
 import { MissingParamError } from '../errors/missing-param-error'
 import { badRequest, ok } from '../helpers/http-helper'
 import { IController } from '../protocols/controller-protocol'
+import { IEmailValidator } from '../protocols/email-validator-protocol'
 import { IHttpRequest, IHttpResponse } from '../protocols/http-protocol'
 
 export class SignUpController implements IController {
+  private readonly emailValidator: IEmailValidator
+  constructor(emailValidator: IEmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
   perform(httpRequest: IHttpRequest): IHttpResponse {
     // check if all required fields were provided to controller
     const requiredFields = [
@@ -24,6 +30,14 @@ export class SignUpController implements IController {
     if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) {
       return badRequest(422, new InvalidParamError('passwordConfirmation'))
     }
-    return ok(201, 'Successful')
+    try {
+      const isValid = this.emailValidator.isValid(httpRequest.body.email)
+      if (!isValid) {
+        return badRequest(422, new InvalidParamError('email'))
+      }
+      return ok(201, 'Successful')
+    } catch (error) {
+      return badRequest(500, new Error('Unexpected Internal Error 500'))
+    }
   }
 }
