45faff7b6fd2ed4dd8d533d441d824940119ef32
João Vitor Santos <joao_vitor.personal@outlook.com>
2022-06-14 17:59:05 -0300

    feat: make SignUpController.perform respect protocols

--- src/presentation/controllers/signup-controller.ts
+++ src/presentation/controllers/signup-controller.ts
@@ -1,10 +1,16 @@
+import { IHttpRequest, IHttpResponse } from '../protocols/http-protocol'
+
 export class SignUpController {
-  perform(httpRequest: any): any {
+  perform(httpRequest: IHttpRequest): IHttpResponse {
     if (!httpRequest.body.name) {
       return {
         statusCode: 400,
         body: 'Missing param: name',
       }
+    }
+    return {
+      statusCode: 201,
+      body: 'Successful',
     }
   }
 }
