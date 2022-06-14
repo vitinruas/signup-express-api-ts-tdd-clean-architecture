8b9eaa62be9d4e058454d1aca8fe63c05e86c620
João Vitor Santos <joao_vitor.personal@outlook.com>
2022-06-14 18:09:07 -0300

    test: ensure SignUpController returns an error as body response

--- src/presentation/controllers/signup-controller.spec.ts
+++ src/presentation/controllers/signup-controller.spec.ts
@@ -15,6 +15,6 @@ describe('SignUpController', () => {
     const httpResponse = sut.perform(httpRequest)
 
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toBe('Missing param: name')
+    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
 })
