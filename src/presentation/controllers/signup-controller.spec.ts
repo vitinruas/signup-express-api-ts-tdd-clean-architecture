ac38009e830e2aa85c19a006e4135f1d88c2ce15
João Vitor Santos <joao_vitor.personal@outlook.com>
2022-06-14 17:39:09 -0300

    test: ensure SignUpController returns a 400 error code if no name is provided

---
+++ src/presentation/controllers/signup-controller.spec.ts
@@ -0,0 +1,19 @@
+import { SignUpController } from './signup-controller'
+
+describe('SignUpController', () => {
+  it('should return a 400 error code if no name is provided', () => {
+    const sut = new SignUpController()
+
+    const httpRequest = {
+      body: {
+        gender: 'any_gender',
+        email: 'any_email',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+      },
+    }
+    const httpResponse = sut.perform(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
--- src/test.test.ts
+++
@@ -1,5 +0,0 @@
-describe('SignUpController', () => {
-  it('should return a 400 error code if no name is provided', () => {
-    expect(1).toBe(1)
-  })
-})
