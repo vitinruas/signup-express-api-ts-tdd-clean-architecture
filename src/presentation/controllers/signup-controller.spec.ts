1b6540e024add0b367ee49d58a002cbe455ed6e7
João Vitor Santos <joao_vitor.personal@outlook.com>
2022-06-14 18:12:47 -0300

    test: ensure SignUpController returns a 400 error code if no gender is provided

--- src/presentation/controllers/signup-controller.spec.ts
+++ src/presentation/controllers/signup-controller.spec.ts
@@ -1,6 +1,7 @@
 import { SignUpController } from './signup-controller'
 
 describe('SignUpController', () => {
+  // return 400 if name wasn't provided
   it('should return a 400 error code if no name is provided', () => {
     const sut = new SignUpController()
 
@@ -16,5 +17,22 @@ describe('SignUpController', () => {
 
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+  })
+  // return 400 if gender wasn't provided
+  it('should return a 400 error code if no gender is provided', () => {
+    const sut = new SignUpController()
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+      },
+    }
+    const httpResponse = sut.perform(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: gender'))
   })
 })
