424dc4a233fbac2ffed91351c874a7d60594972c
João Vitor Santos <joao_vitor.personal@outlook.com>
2022-06-15 12:52:34 -0300

    test: ensure EmailValidatorAdapter calls EmailValidator with correct values

--- src/utils/emailvalidator/email-validator-adapter.spec.ts
+++ src/utils/emailvalidator/email-validator-adapter.spec.ts
@@ -29,4 +29,22 @@ describe('Email Validator', () => {
 
     expect(response).toBe(true)
   })
+  // return true if valid email was provided
+  it('should return true if valid email is provided', async () => {
+    const sut = makeSut()
+
+    const response = await sut.isValid('valid_email')
+
+    expect(response).toBe(true)
+  })
+  // calls Email Validator with correct values
+  it('should calls Email Validator with correct values', async () => {
+    const sut = makeSut()
+
+    const isEmailSpy = jest.spyOn(validator, 'isEmail')
+
+    await sut.isValid('any_email')
+
+    expect(isEmailSpy).toHaveBeenCalledWith('any_email')
+  })
 })
