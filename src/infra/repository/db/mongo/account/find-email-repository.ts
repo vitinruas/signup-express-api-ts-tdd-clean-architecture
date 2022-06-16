3b5aa5a1f0b04269a25512a771567c9f106367d9
João Vitor Santos <joao_vitor.personal@outlook.com>
2022-06-16 14:34:07 -0300

    feat: ensure FindEmailRepository returns undefined if an provided email account doesn't exist

--- src/infra/repository/db/mongo/account/find-email-repository.ts
+++ src/infra/repository/db/mongo/account/find-email-repository.ts
@@ -4,7 +4,7 @@ import { mongoHelper } from '../helpers/mongoHelper'
 export class FindEmailRepository implements IFindEmailRepository {
   async find(email: string): Promise<string | undefined> {
     const collectionRef = mongoHelper.getCollection('accounts')
-    const foundUser = await collectionRef.findOne({ email })
-    return foundUser!.email
+    const response = await collectionRef.findOne({ email })
+    return response ? response!.email : undefined
   }
 }
