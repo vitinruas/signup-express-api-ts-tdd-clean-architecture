f7efa5a0fbc96d10e2afd852d1d25bd47585c846
João Vitor Santos <joao_vitor.personal@outlook.com>
2022-06-16 14:14:20 -0300

    feat: ensure FindEmailRepository find email and returns if it exists

--- src/infra/repository/db/mongo/account/find-email-repository.ts
+++ src/infra/repository/db/mongo/account/find-email-repository.ts
@@ -1,7 +1,10 @@
-import { IFindEmailRepository } from '../../../../data/protocols/check-email/find-email-repository-protocol'
+import { IFindEmailRepository } from '../../../../../data/protocols/check-email/find-email-repository-protocol'
+import { mongoHelper } from '../helpers/mongoHelper'
 
 export class FindEmailRepository implements IFindEmailRepository {
   async find(email: string): Promise<string | undefined> {
-    return Promise.resolve('email')
+    const collectionRef = mongoHelper.getCollection('accounts')
+    const foundUser = await collectionRef.findOne({ email })
+    return foundUser!.email
   }
 }
