ede629670c75497c7905404ac2b4aaa6f3524a80
João Vitor Santos <joao_vitor.personal@outlook.com>
2022-06-17 18:54:33 -0300

    test: ensure Server response with default type as json

---
+++ src/main/middlewares/tests/content-type-middleware.test.ts
@@ -0,0 +1,17 @@
+import request from 'supertest'
+import app from '../../config/app'
+
+describe('Content Type Middleware', () => {
+  test('should return default type as json', async () => {
+    app.post('/test_content_type', (request, response) => {
+      return response.send({
+        iReceive: request.body,
+      })
+    })
+
+    await request(app)
+      .post('/test_content_type')
+      .send('Hello!')
+      .expect('content-type', /json/)
+  })
+})
