754d5c414b4ce0a2901ec2e159dc38ebfed72e41
João Vitor Santos <joao_vitor.personal@outlook.com>
2022-06-17 12:43:12 -0300

    test: ensure Server enables cors

---
+++ src/main/middlewares/tests/cors-middleware.test.ts
@@ -0,0 +1,15 @@
+import request from 'supertest'
+import app from '../../config/app'
+
+describe('CORS Middleware', () => {
+  test('should enable cors', async () => {
+    app.post('/cors_test', (request, response) => {
+      return response.send()
+    })
+    await request(app)
+      .post('/cors_test')
+      .expect('access-controll-allow-headers', '*')
+      .expect('access-controll-allow-origin', '*')
+      .expect('access-controll-allow-methods', '*')
+  })
+})
