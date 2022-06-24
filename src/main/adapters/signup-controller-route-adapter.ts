f9f4762998e388dc5731cef6ca8f3b906d0cc855
João Vitor Santos <joao_vitor.personal@outlook.com>
2022-06-24 19:01:06 -0300

    fix: return error message to Client

--- src/main/adapters/signup-controller-route-adapter.ts
+++ src/main/adapters/signup-controller-route-adapter.ts
@@ -12,6 +12,12 @@ export const adapterExpressToController = (controller: IController) => {
     }
 
     const httpResponse: IHttpResponse = await controller.perform(httpRequest)
+    if (httpResponse.statusCode >= 400) {
+      return response
+        .status(httpResponse.statusCode)
+        .json(httpResponse.body.message)
+        .send()
+    }
     return response
       .status(httpResponse.statusCode)
       .json(httpResponse.body)
