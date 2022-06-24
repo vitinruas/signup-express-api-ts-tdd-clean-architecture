437a0a2276e345c533eb0aa3520e0b9def9eedac
João Vitor Santos <joao_vitor.personal@outlook.com>
2022-06-24 18:52:51 -0300

    feat: ensure LogControllerRepository add a log ocurrency to a database

---
+++ src/infra/repository/db/mongo/system/log-controller/log-controller-repository.ts
@@ -0,0 +1,19 @@
+import { ILogRepository } from '../../../../../../main/decorators/log-controller-decorator-protocols'
+import { mongoHelper } from '../../helpers/mongoHelper'
+
+export class LogControllerRepository implements ILogRepository {
+  async logServerError(stack: string): Promise<void> {
+    const collectionRef = mongoHelper.getCollection('serverErrors')
+
+    const date = new Date().toLocaleDateString('pt-BR', {
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+    })
+
+    await collectionRef.insertOne({
+      stack,
+      date,
+    })
+  }
+}
